f48d25d2fc86bccde24344f1f5db8fe344985c1c
Ricardo <r3soares@gmail.com>
2022-02-06 12:45:21 -0300

    Adicionado CNAES Controllers

---
+++ src/Api/Controllers/Cnaes/ClasseController.cs
@@ -0,0 +1,37 @@
+﻿using CriadorBaseDados.Model.DB;
+using CriadorBaseDados.Model.DB.CNAE;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using src.Respositories;
+using System.Globalization;
+
+namespace Api.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class ClasseController : ControllerBase
+    {
+        private readonly IRepository<CnaeClasse> _repo;
+        private readonly ILogger<ClasseController> _logger;
+        //private readonly CultureInfo dataFormato = CultureInfo.GetCultureInfo("pt-BR");
+
+        public ClasseController(IRepository<CnaeClasse> repo, ILogger<ClasseController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+        public async Task<IActionResult> GetAll()
+        {
+            var t = await _repo.GetAll();
+            return t != null ? Ok(t) : NotFound();
+        }
+
+        //Obtém todas as divisões da seção informada
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSubClasses(string id)
+        {
+            var t = await _repo.GetById(id);
+            return t != null ? Ok(t.Subclasses) : NotFound();
+        }
+    }
+}
---
+++ src/Api/Controllers/Cnaes/GrupoController.cs
@@ -0,0 +1,37 @@
+﻿using CriadorBaseDados.Model.DB;
+using CriadorBaseDados.Model.DB.CNAE;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using src.Respositories;
+using System.Globalization;
+
+namespace Api.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class GrupoController : ControllerBase
+    {
+        private readonly IRepository<CnaeGrupo> _repo;
+        private readonly ILogger<GrupoController> _logger;
+        //private readonly CultureInfo dataFormato = CultureInfo.GetCultureInfo("pt-BR");
+
+        public GrupoController(IRepository<CnaeGrupo> repo, ILogger<GrupoController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+        public async Task<IActionResult> GetAll()
+        {
+            var t = await _repo.GetAll();
+            return t != null ? Ok(t) : NotFound();
+        }
+
+        //Obtém todas as divisões da seção informada
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetClasses(int id)
+        {
+            var t = await _repo.GetById(id);
+            return t != null ? Ok(t.Classes) : NotFound();
+        }
+    }
+}
